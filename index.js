30930a3d5bdf25f9feef5302f8710ff15d0ac13e
Jared Tichacek <jared.tichacek@gmail.com>
2023-02-18 15:43:32 -0600

    added inquirer prompts for engineer/intern

--- index.js
+++ index.js
@@ -5,31 +5,107 @@ const Manager = require('./lib/Manager')
 const Intern = require('./lib/Intern')
 const Engineer = require('./lib/Engineer')
 
-function addManager() {
-inquirer
-.prompt([
-    {
+
+
+let addManager = () => {
+    inquirer
+    .prompt([
+        {
             type: 'input',
             name: 'managerName',
             message: "What is the name of the team manager?"
-    },
+        },
 
-    {
-        type: 'input',
-        name: 'managerId',
-        message: "What is the employee ID of the team manager?"
-    },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the employee ID of the team manager?"
+        },
 
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: "What is the email address of the team manager?"
-    },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the email address of the team manager?"
+        },
 
-    {
-        type: 'input',
-        name: 'managerOfficeNum',
-        message: "What is the office number of the team manager?"
-    },
-])
+        {
+            type: 'input',
+            name: 'managerOfficeNum',
+            message: "What is the office number of the team manager?"
+        },
+    ])
+}
+
+let teamBuilder = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'teamOption',
+            message: "Which team member would you like to add next?",
+            choices: [
+                'Engineer',
+                'Intern',
+                "The team is complete, no additional members are needed."
+            ]
+        }
+    ])
+}
+
+let addEngineer = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is the name of the team engineer?"
+        },
+    
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is the employee ID of the team engineer?"
+        },
+    
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is the email address of the team engineer?"
+        },
+    
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is the GitHub username of the team engineer?"
+        },
+    ])
+}
+
+let addIntern = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is the name of the team intern?"
+        },
+    
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is the employee ID of the team intern?"
+        },
+    
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is the email address of the team intern?"
+        },
+    
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What school did the team intern attend?"
+        },
+    ])
 }
